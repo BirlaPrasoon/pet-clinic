51a87e692bf44173fef75062f28eac2d7cadfbca
Prasoon Birla <prasoon.birla@whatfix.com>
2020-08-09 01:11:18 +0530

    Created Interfaces for Pet Clinic Closes #4

--- pet-clinic-data/src/main/java/com/prasoon/petclinic/data/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/prasoon/petclinic/data/services/OwnerService.java
@@ -5,14 +5,8 @@ import com.prasoon.petclinic.data.model.Owner;
 import java.util.List;
 import java.util.Set;
 
-public interface OwnerService {
-
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner,Long>{
 
     List<Owner> findByLastName(String lastName);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/com/prasoon/petclinic/data/services/VetService.java
+++ pet-clinic-data/src/main/java/com/prasoon/petclinic/data/services/VetService.java
@@ -2,14 +2,6 @@ package com.prasoon.petclinic.data.services;
 
 import com.prasoon.petclinic.data.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
