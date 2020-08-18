ee6bd24996dfff1f6c80297eb042ee33922b2fda
prasoon <birla.prasoon1@gmail.com>
2020-08-19 00:41:25 +0530

    Added specialities, added to Vet. Closes #40

--- pet-clinic-data/src/main/java/com/prasoon/petclinic/data/model/Vet.java
+++ pet-clinic-data/src/main/java/com/prasoon/petclinic/data/model/Vet.java
@@ -2,7 +2,17 @@ package com.prasoon.petclinic.data.model;
 
 import com.prasoon.petclinic.data.model.Person;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
-    public Vet(){}
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
