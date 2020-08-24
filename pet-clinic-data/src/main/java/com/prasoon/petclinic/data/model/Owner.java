9bf433860b3e73d02c247f687a9a4ae14b6b9282
prasoon <birla.prasoon1@gmail.com>
2020-08-25 00:27:23 +0530

    Converted Owners to JPA Entities, Closes #36

--- pet-clinic-data/src/main/java/com/prasoon/petclinic/data/model/Owner.java
+++ pet-clinic-data/src/main/java/com/prasoon/petclinic/data/model/Owner.java
@@ -2,14 +2,24 @@ package com.prasoon.petclinic.data.model;
 
 import lombok.Builder;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
 
-    private Set<Pet> pets;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets ;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
     public Owner() {
