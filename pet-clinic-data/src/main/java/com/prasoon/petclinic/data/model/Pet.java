23b161dc2bd23904bf7847579ec2815c1de09ba0
prasoon <birla.prasoon1@gmail.com>
2020-08-25 00:39:05 +0530

    Converted Visit to Entity, Closes #38

--- pet-clinic-data/src/main/java/com/prasoon/petclinic/data/model/Pet.java
+++ pet-clinic-data/src/main/java/com/prasoon/petclinic/data/model/Pet.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.EnableMBeanExport;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pet")
@@ -22,6 +24,9 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -53,6 +58,14 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     @Override public String toString() {
