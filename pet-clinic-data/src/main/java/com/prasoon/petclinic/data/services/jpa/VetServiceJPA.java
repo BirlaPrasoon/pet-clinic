20896f53b1efcbb48ff15b80301bba9900c4cea7
prasoon <birla.prasoon1@gmail.com>
2020-08-26 12:32:13 +0530

    Added JPA Service for vets, Closes #43

---
+++ pet-clinic-data/src/main/java/com/prasoon/petclinic/data/services/jpa/VetServiceJPA.java
@@ -0,0 +1,35 @@
+package com.prasoon.petclinic.data.services.jpa;
+
+import com.prasoon.petclinic.data.model.Vet;
+import com.prasoon.petclinic.data.repositories.VetRepository;
+import com.prasoon.petclinic.data.services.VetService;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class VetServiceJPA implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceJPA(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override public Collection<Vet> findAll() {
+        Collection<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
